09ea5acc2663359c03cde4d125690d98207194ef
andruvetto <andruvetto@gmail.com>
2015-09-22 15:54:55 -0300

    modifications

--- src/lib/ir/ast/MethodCall.java
+++ src/lib/ir/ast/MethodCall.java
@@ -1,8 +1,22 @@
 package ir.ast;
 
 import ir.ASTVisitor;
+import java.util.List;
 
 public class MethodCall extends Expression {
+    private List<Expression> expressions;
+    private VarLocation location;
+    
+    public MethodCall(VarLocation l, List<Expression> e){
+        expressions = e;
+        location = l;
+    }
+    
+    public MethodCall(VarLocation l){
+        expressions = null;
+        location = l;
+    }
+    
     @Override
     public <T> T accept(ASTVisitor<T> v) {
         return v.visit(this);
